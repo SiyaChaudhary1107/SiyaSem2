57006106b593e7530e90b8a5c785ecb9c11ccca9
SiyaChaudhary1107 <choudharysiya13@gmail.com>
2026-03-19 20:45:05 +0530

    your message

---
+++ switch.js
@@ -0,0 +1,42 @@
+let day = 3 ;
+switch(day){
+    case 1: 
+    console.log("Monday");
+    break;
+
+    case 2: 
+    console.log("Tuesday");
+    break;
+
+    case 3: 
+    console.log("Wednesday");
+    break;
+
+    case 4: 
+    console.log("Thursday");
+    break;
+
+    case 5: 
+    console.log("Friday");
+    break;
+
+    case 6: 
+    console.log("Saturday");
+    break;
+
+    case 7: 
+    console.log("Sunday");
+    break;
+    default:
+    console.log("Invalid");
+    break;
+    
+
+    
+}let age = 2;
+if(age<18){
+    console.log("Not eligible")
+}
+else if(age>18){
+    console.log("Elegible")
+}
